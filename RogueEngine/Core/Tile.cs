06770ca1f22f2864513abaa3efd9f60030a45870
Amit Breiman <crimson.breiman@gmail.com>
2024-02-24 13:21:37 +0200

    Core Start

---
+++ RogueEngine/Core/Tile.cs
@@ -0,0 +1,16 @@
+﻿
+namespace RogueEngine.Core
+{
+    abstract class Tile
+    {
+        // TileObject TileObject;
+        // IPosition position
+        // int OwndedBy -- the player index that owns the tile. if owned by no player -1;
+
+        // full constructor
+        // not owned constructor
+        // blank tile  = not owned, without any object.
+
+
+    }
+}
---
+++ RogueEngine/Core/TileObject.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace RogueEngine.Core
+{
+    abstract class TileObject : ICloneable
+    {
+        // IPosition position
+        // int OwndedBy 
+
+
+
+
+
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
