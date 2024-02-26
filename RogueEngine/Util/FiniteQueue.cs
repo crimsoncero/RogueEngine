4ed379b36136d568d42f6f37672d1c217033cf2b
Amit Breiman <crimson.breiman@gmail.com>
2024-02-26 06:20:09 +0200

    Updated Finite Queue

--- RogueEngine/Util/FiniteQueue.cs
+++ RogueEngine/Util/FiniteQueue.cs
@@ -2,46 +2,22 @@
 
 namespace RogueEngine.Util
 {
-    class FiniteQueue<T>
+    class FiniteQueue<T> : Queue<T>
     {
-        private Queue<T> _queue;
         public int Capacity { get; init; }
 
-        public int Count { get { return _queue.Count; } }
-        public T this[int i] { get { return ElementAt(i); } }
-        
-
-        public FiniteQueue(int capacity)
+        public FiniteQueue(int capacity) : base(capacity)
         {
-            _queue = new Queue<T>();
             Capacity = capacity;
         }
 
-        public void Enqueue(T item)
+        public new void Enqueue(T item)
         {
-            _queue.Enqueue(item);
-            if (_queue.Count > Capacity)
-                _queue.Dequeue();
-        }
-
-        public T Dequeue()
-        {
-            return _queue.Dequeue();
-        }
-
-        public T Peek()
-        {
-            return _queue.Peek();
-        }
-
-        public T ElementAt(int index)
-        {
-            return _queue.ElementAt(index);
-        }
-
-        public void Clear()
-        {
-            _queue.Clear();
+            base.Enqueue(item);
+            if(Capacity > Count)
+            {
+                Dequeue();
+            }
         }
 
     }
