4c907b14fda3b190bf60c8528546403a0566906d
Amit Breiman <crimson.breiman@gmail.com>
2024-02-26 06:43:31 +0200

    FiniteQueue inherits Queue and "overrides" Enqueue

--- ChessDemo/Program.cs
+++ ChessDemo/Program.cs
@@ -1,4 +1,20 @@
 ﻿using RogueEngine;
 using RogueEngine.Renderer;
+using RogueEngine.Util;
 
-Console.WriteLine("Hello there");
+var log = new FiniteQueue<int>(5);
+
+log.Enqueue(0);
+log.Enqueue(1);
+log.Enqueue(2);
+log.Enqueue(3);
+log.Dequeue();
+log.Enqueue(4);
+log.Enqueue(5);
+log.Enqueue(6);
+log.Enqueue(7);
+log.Enqueue(8);
+log.Enqueue(9);
+log.Enqueue(10);
+
+
--- RogueEngine/Util/FiniteQueue.cs
+++ RogueEngine/Util/FiniteQueue.cs
@@ -2,11 +2,11 @@
 
 namespace RogueEngine.Util
 {
-    class FiniteQueue<T> : Queue<T>
+    public class FiniteQueue<T> : Queue<T>
     {
         public int Capacity { get; init; }
 
-        public FiniteQueue(int capacity) : base(capacity)
+        public FiniteQueue(int capacity) : base()
         {
             Capacity = capacity;
         }
@@ -14,9 +14,9 @@ namespace RogueEngine.Util
         public new void Enqueue(T item)
         {
             base.Enqueue(item);
-            if(Capacity > Count)
+            if (Count > Capacity)
             {
-                Dequeue();
+               Dequeue();
             }
         }
 
