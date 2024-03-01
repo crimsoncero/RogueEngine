aacb9fc42e3edf5690b2e0c93bbd63520a66e9c4
Leeshap98 <1998lee@gmail.com>
2024-03-01 12:08:24 +0200

    Window methods

--- RogueEngine/Renderer/Console/Window.cs
+++ RogueEngine/Renderer/Console/Window.cs
@@ -2,6 +2,8 @@
 
 namespace RogueEngine.Renderer.Console
 {
+    using System;
+
     public abstract class Window
     {
         //const char[] _border = new char[] { };
@@ -28,13 +30,41 @@ namespace RogueEngine.Renderer.Console
 
         public virtual void RenderWindow()
         {
-            // Print a blank window with edges 
-            /* Right Border: 221  ▌
-             * Left Border:222 ▐
-             * Top Border: 223 ▀
-             * Bot Border: 220 ▄
-             */
+            char topBorder = '▀', bottomBorder = '▄', leftBorder = '▐', rightBorder = '▌';
+            string topBottomRow = topBorder.ToString().PadRight(Width - 1, topBorder) + topBorder;
+            string middleRow = leftBorder + new string(' ', Width - 2) + rightBorder;
+
+            Console.WriteLine(topBottomRow); // Top border
+            for (int i = 1; i < Height - 1; i++)
+            {
+                Console.WriteLine(middleRow); // Middle rows
+            }
+            Console.WriteLine(topBottomRow); // Bottom border
+
         }
+
+        public void ClearWindow(bool clearFrame = false)
+        {
+            Console.SetCursorPosition(TopLeftAnchor.X, TopLeftAnchor.Y);
+
+            if (clearFrame)
+            {
+                for (int i = 0; i < Height; i++)
+                {
+                    Console.WriteLine(new string(' ', Width));
+                }
+            }
+            else
+            {
+                for (int row = 1; row < Height - 1; row++)
+                {
+                    Console.SetCursorPosition(TopLeftAnchor.X + 1, TopLeftAnchor.Y + row);
+                    Console.Write(new string(' ', Width - 2));
+                }
+            }
+        }
+
+
         public override string ToString()
         {
             return $"Window [width={Width}, height={Height}, layerOrder={LayerOrder}, topLeftAnchor={TopLeftAnchor}]";
