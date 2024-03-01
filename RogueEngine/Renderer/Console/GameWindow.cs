b2f89d345f6b242415e1eac489d99216f6f3fd3f
Leeshap98 <1998lee@gmail.com>
2024-03-01 12:12:44 +0200

    Gamewindow methods

--- RogueEngine/Renderer/Console/GameWindow.cs
+++ RogueEngine/Renderer/Console/GameWindow.cs
@@ -2,12 +2,14 @@
 
 namespace RogueEngine.Renderer.Console
 {
+    using System;
     public class GameWindow : Window
     {
         public int TilemapIndex { get; set; }
         public char[] RowNames { get; set; }
         public char[] ColumnNames { get; set; }
         public bool RenderGuidelines { get; set; }
+        private char[,] Tilemap;
 
         public GameWindow(int width, int height, int layerOrder, IPosition topLeftAnchor, int tilemapIndex,
             char[] rowNames, char[] columnNames, bool renderGuidelines) : base(width, height, layerOrder, topLeftAnchor)
@@ -16,6 +18,28 @@ namespace RogueEngine.Renderer.Console
             RowNames = rowNames;
             ColumnNames = columnNames;
             RenderGuidelines = renderGuidelines;
+        }
+
+
+        public GameWindow(char[,] tilemap, int layerOrder, IPosition topLeftAnchor, int tilemapIndex,
+            char[] rowNames, char[] columnNames, bool renderGuidelines) : base(tilemap.GetLength(1), tilemap.GetLength(0), layerOrder, topLeftAnchor)
+        {
+            Tilemap = tilemap;
+            TilemapIndex = tilemapIndex;
+            RowNames = rowNames;
+            ColumnNames = columnNames;
+            RenderGuidelines = renderGuidelines;
+        }
+
+        public void DrawTile(IPosition position, char tile)
+        {
+            Console.SetCursorPosition(position.X, position.Y);
+            Console.Write(tile);
+        }
+
+        public void DrawObject(IPosition position, char tileObject)
+        {
+            DrawTile(position, tileObject);
         }
 
 
