31539c7130916abac73b1a9c8a373bf1df87d62d
Amit Breiman <crimson.breiman@gmail.com>
2024-03-02 18:21:45 +0200

    PathMaker

---
+++ RogueEngine/Movements/PathMaker.cs
@@ -0,0 +1,46 @@
+﻿
+namespace RogueEngine.Movements
+{
+    /// <summary>
+    /// A Static utility class to help create more complicated Path Collections.
+    /// </summary>
+    public class PathMaker
+    {
+        public static Path Line(PathDirections direction, int length)
+        {
+            return Path.Create(direction, length);
+        }
+
+
+
+        public static ICollection<Path> EightDirectional(int length)
+        {
+            throw new NotImplementedException();
+        }
+        public static ICollection<Path> OrthagonalAll(int length)
+        {
+            throw new NotImplementedException(); 
+        }
+        public static ICollection<Path> DiagonalAll(int length)
+        {
+            throw new NotImplementedException();
+        }
+        public static ICollection<Path> OrthagonalUp(int length)
+        {
+            throw new NotImplementedException();
+        }
+        public static ICollection<Path> OrthagonalDown(int length)
+        {
+            throw new NotImplementedException();
+        }
+        public static ICollection<Path> DiagonalUp(int length)
+        {
+            throw new NotImplementedException();
+        }
+        public static ICollection<Path> DiagonalDown(int length)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
