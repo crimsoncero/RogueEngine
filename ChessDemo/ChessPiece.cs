4dafa8acd1a07b38a89c700ae029ae0b5d5e94e5
Amit Breiman <93070078+crimsoncero@users.noreply.github.com>
2024-03-11 18:00:05 +0200

    2

---
+++ ChessDemo/ChessPiece.cs
@@ -0,0 +1,24 @@
+﻿using Path = RogueEngine.Movements.Path;
+
+namespace ChessDemo
+{
+    class ChessPiece : TileObject
+    {
+        private ChessPiece(int ownedBy) : base(ownedBy)
+        {
+        }
+
+        public static ChessPiece CreateRook(bool isWhite)
+        {
+            ChessPiece piece = new ChessPiece(isWhite ? 0 : 1);
+            
+            piece.Renderer = new TOConsoleRenderer('R', isWhite ? ConsoleColor.DarkBlue : ConsoleColor.DarkRed, ConsoleColor.Black, true);
+
+            var paths = PathMaker.OrthogonalAll(7);
+            piece.Movement = new Movement(paths);
+
+            return piece;
+        }
+
+    }
+}
