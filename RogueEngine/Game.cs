f9376c089c2ade27914464e4d16cc4d091131157
Amit Breiman <crimson.breiman@gmail.com>
2024-03-26 13:54:16 +0200

    11

--- RogueEngine/Game.cs
+++ RogueEngine/Game.cs
@@ -1,13 +1,50 @@
 ﻿namespace RogueEngine
 {
-    public class Game<T> where T : IRenderer
+    public class Game<T> where T : IRenderer, new()
     {
 
         public T Renderer { get; set; }
+        public CommandHandler CommandHandler { get; set; }
 
-        public static void Foo()
+        /// <summary>
+        /// The condition to end the game, return the index of the winner, -1 if the game hasn't ended, or -2 in case of a tie.
+        /// </summary>
+        public Func<Tilemap, int> EndCondition { get; set; }
+        public Func<Tilemap> InitTilemap { get; set; }
+        
+        private TileObject _selectedObject = null;
+        private Tilemap _tilemap = null;
+
+        private Game()
         {
-            Console.WriteLine("Hello World");
+            Renderer = new T();
+
+        }
+
+
+        public void Start()
+        {
+            // Render
+
+            // Await Command
+
+            // Update Game state according to command
+
+            // Check End Condition
+
+            // REPEAT OR EXIT
+
+        }
+
+
+
+
+        public static Game<T> Create()
+        {
+            Game<T> game = new Game<T>();
+            game.InitTilemap();
+
+            return game;
         }
     }
 }
--- RogueEngine/Using.cs
+++ RogueEngine/Using.cs
@@ -4,5 +4,5 @@ global using RogueEngine.Positions;
 global using RogueEngine.Renderer;
 global using RogueEngine.Util;
 global using RogueEngine.Renderer.Console;
-
+global using RogueEngine.Commands;
 
